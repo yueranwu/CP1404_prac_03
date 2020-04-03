0cb67aad23675a31d5c49aa883ee827cafdf8afa
Yueran <yueran.wu@my.jcu.edu.au>
2020-04-03 18:31:01 +0800

    Refactor password check program to use functions

--- password_check.py
+++ password_check.py
@@ -1,16 +1,21 @@
-def set_password(length):
+def main():
+    length = 8
+    password = get_password(length)
+    print_asterisks(password)
+
+
+def get_password(length):
     password = input("Please enter your password: ")
     while len(password) < length:
         print("Your password should be {} characters".format(length))
         password = input("Please enter your password: ")
+    return password
+
+
+def print_asterisks(password):
     print("Your password is:")
     for char in password:
         print("*", end='')
 
 
-def main():
-    set_password(12)
-
-
-if __name__ == '__main__':
-    main()
+main()
