f29856ed4b1c47a861ced5def4ac6ec2890beb38
Yueran <yueran.wu@my.jcu.edu.au>
2020-04-03 20:36:38 +0800

    Refactor score check program to functions Add random score generation function

--- broken_score.py
+++ broken_score.py
@@ -5,12 +5,25 @@ The intention is that the score must be between 0 and 100 inclusive;
 90 or more is excellent;
 50 or more is a pass; below 50 is bad.
 """
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif 50 <= score < 90:
-    print("Passable")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(result(score))
+    score = random.randint(0, 100)
+    print(result(score))
+
+
+def result(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif 50 <= score < 90:
+        return "Passable"
+    elif score >= 90:
+        return "Excellent"
+    else:
+        return "Bad"
+
+
+main()
